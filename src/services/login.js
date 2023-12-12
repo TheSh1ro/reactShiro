7423ac8d56d003bba8ef31735d0b0b20171b4b19
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-12-12 20:25:59 -0300

    Remoção do console.log

--- src/services/login.js
+++ src/services/login.js
@@ -4,7 +4,6 @@ class LoginApi {
   async login(user) {
     try {
       const { data } = await api.post("/token/", user);
-      console.log(data);
       return data; // Retorna diretamente os dados da resposta
     } catch (error) {
       console.error("Erro ao fazer login:", error);
